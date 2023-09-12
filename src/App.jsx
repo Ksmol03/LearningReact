51dea0d94dc745aadda898002afd693b63a69ae3
Kacper Smoliński <kacper.smolinski@hotmail.com>
2023-09-12 16:41:59 +0200

    Moved useState from Button component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <Button />
-      <Button />
-    </>
-  )
-}
-
-function Button() {
   const [count, setCount] = useState(0);
-
+  
   function handleClick() {
     setCount(count + 1)
   }
 
   return (
-    <button onClick={handleClick}>Clicked {count} times</button>
+    <>
+      <Button onClick={handleClick} count={count}/>
+      <Button onClick={handleClick} count={count}/>
+    </>
+  )
+}
+
+function Button({onClick, count}) {
+
+  return (
+    <button onClick={onClick}>Clicked {count} times</button>
   )
 }
 
